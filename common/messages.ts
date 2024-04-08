a9aaafd2577442ee31297adfb22a5e407374e624
Sean <ussa.say@gmail.com>
2024-04-07 18:27:09 -0700

    docs: Discriminated union example

---
+++ common/messages.ts
@@ -0,0 +1,16 @@
+/**
+ * Example of a discriminated union to represent the types of messages sent
+ * between the server and client
+ */
+export type ServerMessage =
+  | { type: 'ping' }
+  | { type: 'pong' }
+  | {
+      type: 'usernames'
+      usernames: string[]
+    }
+  | {
+      type: 'set-size'
+      width: number
+      height: number
+    }
