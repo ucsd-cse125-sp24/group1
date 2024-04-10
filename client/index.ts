8daabbcabf669284b40e3036a9ac0d20f70821fd
William Wu <dev.wwu@gmail.com>
2024-04-09 22:56:52 -0700

    style: add prettier, format files

--- client/index.ts
+++ client/index.ts
@@ -1,90 +1,90 @@
-import './index.css'
-import './net/network'
-import vertexShaderSource from './shaders/hello-world.vert'
-import fragmentShaderSource from './shaders/hello-world.frag'
+import "./index.css";
+import "./net/network";
+import vertexShaderSource from "./shaders/hello-world.vert";
+import fragmentShaderSource from "./shaders/hello-world.frag";
 
-const canvas = document.getElementById('canvas')
+const canvas = document.getElementById("canvas");
 if (!(canvas instanceof HTMLCanvasElement)) {
-  throw new TypeError('No <canvas> element present')
+  throw new TypeError("No <canvas> element present");
 }
 
 // Set <canvas> size to the size of the page
 const observer = new ResizeObserver(([{ contentBoxSize }]) => {
-  const [{ blockSize, inlineSize }] = contentBoxSize
-  canvas.width = blockSize
-  canvas.height = inlineSize
-})
-observer.observe(canvas)
+  const [{ blockSize, inlineSize }] = contentBoxSize;
+  canvas.width = blockSize;
+  canvas.height = inlineSize;
+});
+observer.observe(canvas);
 
-const gl = canvas.getContext('webgl')
+const gl = canvas.getContext("webgl");
 if (!gl) {
-  throw new TypeError('Failed to get WebGL context')
+  throw new TypeError("Failed to get WebGL context");
 }
 
-const vertexShader = createShader(gl, 'vertex', vertexShaderSource)
-const fragmentShader = createShader(gl, 'fragment', fragmentShaderSource)
-const program = createProgram(gl, vertexShader, fragmentShader)
+const vertexShader = createShader(gl, "vertex", vertexShaderSource);
+const fragmentShader = createShader(gl, "fragment", fragmentShaderSource);
+const program = createProgram(gl, vertexShader, fragmentShader);
 
-const rotationUniformLocation = gl.getUniformLocation(program, 'u_rotation')
-const positionAttributeLocation = gl.getAttribLocation(program, 'a_position')
-const positionBuffer = gl.createBuffer()
-gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer)
+const rotationUniformLocation = gl.getUniformLocation(program, "u_rotation");
+const positionAttributeLocation = gl.getAttribLocation(program, "a_position");
+const positionBuffer = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 gl.bufferData(
   gl.ARRAY_BUFFER,
   new Float32Array([0, 0, 0, 0.5, 0.7, 0]),
   gl.STATIC_DRAW
-)
+);
 
 const paint = () => {
-  gl.viewport(0, 0, gl.canvas.width, gl.canvas.height)
-  gl.clearColor(0, 0, 0, 0)
-  gl.clear(gl.COLOR_BUFFER_BIT)
-  gl.useProgram(program)
-  gl.uniform1f(rotationUniformLocation, (Date.now() / 200) % (2 * Math.PI))
-  gl.enableVertexAttribArray(positionAttributeLocation)
-  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer)
-  gl.vertexAttribPointer(positionAttributeLocation, 2, gl.FLOAT, false, 0, 0)
-  gl.drawArrays(gl.TRIANGLES, 0, 3)
+  gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+  gl.clearColor(0, 0, 0, 0);
+  gl.clear(gl.COLOR_BUFFER_BIT);
+  gl.useProgram(program);
+  gl.uniform1f(rotationUniformLocation, (Date.now() / 200) % (2 * Math.PI));
+  gl.enableVertexAttribArray(positionAttributeLocation);
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+  gl.vertexAttribPointer(positionAttributeLocation, 2, gl.FLOAT, false, 0, 0);
+  gl.drawArrays(gl.TRIANGLES, 0, 3);
 
-  window.requestAnimationFrame(paint)
-}
-paint()
+  window.requestAnimationFrame(paint);
+};
+paint();
 
-function createShader (
+function createShader(
   gl: WebGLRenderingContext,
-  type: 'vertex' | 'fragment',
+  type: "vertex" | "fragment",
   source: string
 ): WebGLShader {
   const shader = gl.createShader(
-    type === 'vertex' ? gl.VERTEX_SHADER : gl.FRAGMENT_SHADER
-  )
+    type === "vertex" ? gl.VERTEX_SHADER : gl.FRAGMENT_SHADER
+  );
   if (shader) {
-    gl.shaderSource(shader, source)
-    gl.compileShader(shader)
+    gl.shaderSource(shader, source);
+    gl.compileShader(shader);
     if (gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-      return shader
+      return shader;
     }
-    console.error(gl.getShaderInfoLog(shader))
+    console.error(gl.getShaderInfoLog(shader));
   }
-  gl.deleteShader(shader)
-  throw new TypeError('Failed to create shader')
+  gl.deleteShader(shader);
+  throw new TypeError("Failed to create shader");
 }
 
-function createProgram (
+function createProgram(
   gl: WebGLRenderingContext,
   vertexShader: WebGLShader,
   fragmentShader: WebGLShader
 ): WebGLProgram {
-  const program = gl.createProgram()
+  const program = gl.createProgram();
   if (program) {
-    gl.attachShader(program, vertexShader)
-    gl.attachShader(program, fragmentShader)
-    gl.linkProgram(program)
+    gl.attachShader(program, vertexShader);
+    gl.attachShader(program, fragmentShader);
+    gl.linkProgram(program);
     if (gl.getProgramParameter(program, gl.LINK_STATUS)) {
-      return program
+      return program;
     }
-    console.error(gl.getProgramInfoLog(program))
+    console.error(gl.getProgramInfoLog(program));
   }
-  gl.deleteProgram(program)
-  throw new TypeError('Failed to create program')
+  gl.deleteProgram(program);
+  throw new TypeError("Failed to create program");
 }
--- server/index.ts
+++ server/index.ts
@@ -13,53 +13,55 @@ const wss = new WebSocketServer({ server });
 
 app.use(express.static(path.join(__dirname, "..", "public")));
 
-app.get("/", (req,res)=>{
-	res.sendFile(path.join(__dirname, "public/index.html"));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
-wss.on("connection", ws => {
-	const response: ServerMessage = {
-		type: "ping"
-	};
-	ws.send(JSON.stringify(response));
-	ws.on("message", rawData => {
-		let stringData = Array.isArray(rawData) ? rawData.join("") : rawData.toString();
-		console.log(stringData);
-		let data: ClientMessage;
-		try {
-			data = JSON.parse(stringData);
-		} catch {
-			console.warn("Non-JSON message: ", stringData);
-			return;
-		}
-		let response = handleMessage(data);
-		if (response) {
-			ws.send(JSON.stringify(response));
-		}
-	});
+wss.on("connection", (ws) => {
+  const response: ServerMessage = {
+    type: "ping",
+  };
+  ws.send(JSON.stringify(response));
+  ws.on("message", (rawData) => {
+    const stringData = Array.isArray(rawData)
+      ? rawData.join("")
+      : rawData.toString();
+    console.log(stringData);
+    let data: ClientMessage;
+    try {
+      data = JSON.parse(stringData);
+    } catch {
+      console.warn("Non-JSON message: ", stringData);
+      return;
+    }
+    const response = handleMessage(data);
+    if (response) {
+      ws.send(JSON.stringify(response));
+    }
+  });
 });
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 function broadcast(wss: WebSocketServer, message: ServerMessage) {
-	for (let ws of wss.clients) {
-		ws.send(JSON.stringify(message));
-	}
+  for (const ws of wss.clients) {
+    ws.send(JSON.stringify(message));
+  }
 }
 
 function handleMessage(data: ClientMessage): ServerMessage | undefined {
-	switch (data.type) {
-		case "ping":
-			return {
-				type: "pong"
-			};
-		case "pong":
-			return {
-				type: "ping"
-			}
-		case "set-size":
-			
-		break;
-	}
-	return;
+  switch (data.type) {
+    case "ping":
+      return {
+        type: "pong",
+      };
+    case "pong":
+      return {
+        type: "ping",
+      };
+    case "set-size":
+      break;
+  }
+  return;
 }
 
 server.listen(6969);
