b22a738867120556bc3be000f3150f8c43b0557f
Sean <ussa.say@gmail.com>
2024-04-12 17:44:05 -0700

    refactor: Add buffer views
    for Nick

--- client/model/gltf-parser.ts
+++ client/model/gltf-parser.ts
@@ -1,22 +1,29 @@
 type Gltf = {
-	asset: {
-		version: string;
-		minVersion?: string;
-		generator?: string;
-		copyright?: string;
-	};
 	scenes: {
 		nodes: number[];
 	}[];
-	/** Should exist; can default to 0. */
-	scene?: number;
-	nodes?: {
-		children?: number[];
+	scene: number;
+	nodes: {
+		children: number[];
 		translation?: [x: number, y: number, z: number];
 		/** A quaternion */
 		rotation?: [x: number, y: number, z: number, w: number];
 		scale?: [x: number, y: number, z: number];
 		/** Length 16 */
 		matrix?: number[];
+	}[];
+	buffers: {
+		byteLength: number;
+		/** May be a data URI */
+		uri: string;
+	}[];
+	bufferViews: {
+		buffer: number;
+		byteOffset: number;
+		byteLength: number;
+		/** For vertex attributes only */
+		byteStride?: number;
+		/** For vertex indices and attributes */
+		target: WebGL2RenderingContext["ELEMENT_ARRAY_BUFFER"] | WebGL2RenderingContext["ARRAY_BUFFER"];
 	}[];
 };
