a7bcb76a5cdf873fea960188c58b212b38acfafd
William Wu <dev.wwu@gmail.com>
2024-05-17 11:16:08 -0700

    refactor: Put movement code in `PlayerEntity` and resolve circular dependency

--- server/entities/BossEntity.ts
+++ server/entities/BossEntity.ts
@@ -1,159 +1,41 @@
-import * as phys from "cannon-es";
-import { MovementInfo, Vector3 } from "../../common/commontypes";
-import { EntityModel, SerializedEntity } from "../../common/messages";
-import { PlayerMaterial } from "../materials/SourceMaterials";
-import { TheWorld } from "../physics";
+import { Vector3 } from "../../common/commontypes";
+import { EntityModel } from "../../common/messages";
 import { PlayerEntity } from "./PlayerEntity";
-import { Entity } from "./Entity";
 
-//LITERALLY A COPY OF THE HERO ENTITY RIGHT NOW
-//TODO FIX THIS!
+const PLAYER_INTERACTION_RANGE = 2.0;
+const BOSS_CAPSULE_HEIGHT = 2;
+const BOSS_CAPSULE_RADIUS = 0.5;
+
+const BOSS_WALK_SPEED = 20;
+/**
+ * Maximum change in horizontal velocity that can be caused by the player in one
+ * tick
+ */
+const MAX_BOSS_GROUND_SPEED_CHANGE = 3;
+/** Maximum change in horizontal velocity that can occur while in the air */
+const MAX_BOSS_AIR_SPEED_CHANGE = 1;
+const BOSS_JUMP_SPEED = 10;
 
 export class BossEntity extends PlayerEntity {
-	type: string;
-	name: string;
-	body: phys.Body;
-	model: EntityModel[];
-
 	// Game properties
-	speed: number;
 	jumping: boolean;
 
-	// shapes
-	cylinder: phys.Cylinder;
-	sphereTop: phys.Sphere;
-	sphereBot: phys.Sphere;
-	onGround: boolean;
-
 	constructor(name: string, pos: Vector3, model: EntityModel[] = []) {
-		super(name, pos, model, 120, 3, 2.0);
-
-
-
-		this.type = "player-hero";
-		this.name = name;
-		this.model = model;
-
-		// Magic numbers!!! WOOHOO
-		this.speed = 120;
-		this.jumping = false;
-		this.onGround = false;
-
-		this.body = new phys.Body({
-			mass: 1.0, //fuckable
-			position: new phys.Vec3(...pos),
-			fixedRotation: true,
-			material: PlayerMaterial,
-		});
-
-		// Add player cylinder
-		this.cylinder = new phys.Cylinder(
-			0.25, // Smaller top radius
-			0.25, // Smaller bottom radius
-			0.6,  // Shorter height
-			12,
+		super(
+			name,
+			pos,
+			model,
+			10,
+			BOSS_CAPSULE_HEIGHT,
+			BOSS_CAPSULE_RADIUS,
+			BOSS_WALK_SPEED,
+			MAX_BOSS_GROUND_SPEED_CHANGE,
+			MAX_BOSS_AIR_SPEED_CHANGE,
+			BOSS_JUMP_SPEED,
+			PLAYER_INTERACTION_RANGE,
 		);
 
-		this.sphereTop = new phys.Sphere(0.15); // Smaller sphere for the top
-		this.sphereBot = new phys.Sphere(0.15); // Smaller sphere for the bottom
-
-		this.body.addShape(this.cylinder);
-		this.body.addShape(this.sphereTop, new phys.Vec3(0, 0.3, 0));
-		this.body.addShape(this.sphereBot, new phys.Vec3(0, -0.3, 0));
-	}
-
-	move(movement: MovementInfo) {
-		//this is bugged!
-		this.checkOnGround();
-
-		let forwardVector = new phys.Vec3(movement.lookDir[0], 0, movement.lookDir[2]);
-		forwardVector.normalize();
-
-		let rightVector = forwardVector.cross(new phys.Vec3(0, 1, 0));
-
-		let movementVector = new phys.Vec3(0, 0, 0);
-
-		if (movement.forward) {
-			movementVector = movementVector.vadd(forwardVector);
-		}
-		if (movement.backward) {
-			movementVector = movementVector.vadd(forwardVector.negate());
-		}
-		if (movement.right) {
-			movementVector = movementVector.vadd(rightVector);
-		}
-		if (movement.left) {
-			movementVector = movementVector.vadd(rightVector.negate());
-		}
-
-		movementVector.normalize();
-
-		// if (movement.forward) {
-		// 	console.log(forwardVector);
-		// 	console.log(movementVector);
-		// }
-
-		// if (movement.jump) console.log("jump");
-		// if (this.onGround) console.log("based");
-
-		if (movement.jump && this.onGround) {
-			// chatGPT for debug string
-			const stringsArray = ["weeeee", "yahooooo", "mario", "yap", "hawaii"];
-			const randomIndex = Math.floor(Math.random() * stringsArray.length);
-			const randomString = stringsArray[randomIndex];
-			console.log(randomString);
-		}
-
-		if (this.body.force.length() < 1) {
-			this.body.applyForce(movementVector.scale(this.speed));
-		}
-	}
-
-	checkOnGround(): void {
-		// apparently this generate a ray segment and only check intersection within that segment
-		const checkerRay = new phys.Ray(this.body.position, this.body.position.vadd(new phys.Vec3(0, -1, 0)));
-		const result = TheWorld.castRay(checkerRay, {
-			collisionFilterMask: Entity.ENVIRONMENT_COLLISION_GROUP,
-			checkCollisionResponse: false,
-		});
-		// console.log(checkerRay);
-		// console.log(result);
-
-		this.onGround = false;
-		if (result.hasHit) {
-			if (result.distance <= 0.5 + Entity.EPSILON) {
-				this.onGround = true;
-			}
-		}
-	}
-
-	override onCollide(otherEntity: Entity): void {}
-
-	serialize(): SerializedEntity {
-		return {
-			name: this.name,
-			model: this.model,
-			position: this.body.position.toArray(),
-			quaternion: this.body.quaternion.toArray(),
-			colliders: [
-				{
-					type: "cylinder",
-					radiusTop: this.cylinder.radiusTop,
-					radiusBottom: this.cylinder.radiusBottom,
-					height: this.cylinder.height,
-					numSegments: this.cylinder.numSegments,
-				},
-				{
-					type: "sphere", //capsule Top
-					radius: this.sphereTop.radius,
-					offset: [0, 0.25, 0],
-				},
-				{
-					type: "sphere", //capsule Bot
-					radius: this.sphereBot.radius,
-					offset: [0, -0.25, 0],
-				},
-			],
-		};
+		this.type = "player-boss";
+		this.jumping = false;
 	}
 }
--- server/entities/HeroEntity.ts
+++ server/entities/HeroEntity.ts
@@ -1,133 +1,41 @@
-import * as phys from "cannon-es";
-import { MovementInfo, Vector3 } from "../../common/commontypes";
-import { EntityModel, SerializedEntity } from "../../common/messages";
+import { Vector3 } from "../../common/commontypes";
+import { EntityModel } from "../../common/messages";
 import { PlayerEntity } from "./PlayerEntity";
-import { Entity } from "./Entity";
-import { Item } from "./Interactable/Item";
 
 const PLAYER_INTERACTION_RANGE = 2.0;
-const PLAYER_CAPSULE_HEIGHT = 2;
-const PLAYER_CAPSULE_RADIUS = 0.5;
-const cylinderHeight = PLAYER_CAPSULE_HEIGHT - 2 * PLAYER_CAPSULE_RADIUS;
+const HERO_CAPSULE_HEIGHT = 2;
+const HERO_CAPSULE_RADIUS = 0.5;
 
-const MAX_WALK_SPEED = 20;
+const HERO_WALK_SPEED = 20;
 /**
  * Maximum change in horizontal velocity that can be caused by the player in one
  * tick
  */
-const MAX_USER_VELOCITY_CHANGE = 3;
+const MAX_HERO_GROUND_SPEED_CHANGE = 3;
 /** Maximum change in horizontal velocity that can occur while in the air */
-const MAX_VELOCITY_CHANGE_IN_AIR = 1;
-const JUMP_SPEED = 10;
+const MAX_HERO_AIR_SPEED_CHANGE = 1;
+const HERO_JUMP_SPEED = 10;
 
 export class HeroEntity extends PlayerEntity {
 	// Game properties
 	jumping: boolean;
 
-	// shapes
-	cylinder: phys.Cylinder;
-	sphereTop: phys.Sphere;
-	sphereBot: phys.Sphere;
-
 	constructor(name: string, pos: Vector3, model: EntityModel[] = []) {
-		super(name, pos, model, MAX_WALK_SPEED, 10, PLAYER_INTERACTION_RANGE, cylinderHeight + PLAYER_CAPSULE_RADIUS);
+		super(
+			name,
+			pos,
+			model,
+			10,
+			HERO_CAPSULE_HEIGHT,
+			HERO_CAPSULE_RADIUS,
+			HERO_WALK_SPEED,
+			MAX_HERO_GROUND_SPEED_CHANGE,
+			MAX_HERO_AIR_SPEED_CHANGE,
+			HERO_JUMP_SPEED,
+			PLAYER_INTERACTION_RANGE,
+		);
 
 		this.type = "player-hero";
 		this.jumping = false;
-
-		// Add player cylinder
-		this.cylinder = new phys.Cylinder(
-			PLAYER_CAPSULE_RADIUS, // Top radius
-			PLAYER_CAPSULE_RADIUS, // Bottom radius
-			cylinderHeight, // Height
-			12, // Number of Cylinder segments
-		);
-
-		this.sphereTop = new phys.Sphere(PLAYER_CAPSULE_RADIUS);
-		this.sphereBot = new phys.Sphere(PLAYER_CAPSULE_RADIUS);
-
-		this.body.addShape(this.cylinder, new phys.Vec3(0, -cylinderHeight / 2, 0));
-
-		// Add player capsule top
-		this.body.addShape(this.sphereTop);
-
-		// Add player capsule bottom
-		this.body.addShape(this.sphereBot, new phys.Vec3(0, -cylinderHeight, 0));
-	}
-
-	move(movement: MovementInfo) {
-		this.lookDir = new phys.Vec3(...movement.lookDir);
-
-		this.checkOnGround();
-
-		const forwardVector = new phys.Vec3(movement.lookDir[0], 0, movement.lookDir[2]);
-		forwardVector.normalize();
-		const rightVector = forwardVector.cross(new phys.Vec3(0, 1, 0));
-		const currentVelocity = this.body.velocity;
-		const maxChange = this.onGround ? MAX_USER_VELOCITY_CHANGE : MAX_VELOCITY_CHANGE_IN_AIR;
-
-		let targetVelocity = new phys.Vec3(0, 0, 0);
-		if (movement.forward) {
-			targetVelocity = targetVelocity.vadd(forwardVector);
-		}
-		if (movement.backward) {
-			targetVelocity = targetVelocity.vadd(forwardVector.negate());
-		}
-		if (movement.right) {
-			targetVelocity = targetVelocity.vadd(rightVector);
-		}
-		if (movement.left) {
-			targetVelocity = targetVelocity.vadd(rightVector.negate());
-		}
-		if (targetVelocity.length() > 0) {
-			targetVelocity.normalize();
-		}
-		targetVelocity = targetVelocity.scale(this.speed);
-
-		let deltaVelocity = targetVelocity.vsub(currentVelocity.vmul(new phys.Vec3(1, 0, 1)));
-		if (deltaVelocity.length() > maxChange) {
-			deltaVelocity = deltaVelocity.scale(maxChange / deltaVelocity.length());
-		}
-		this.body.applyImpulse(deltaVelocity.scale(this.body.mass));
-
-		if (this.itemInHands instanceof Item) {
-			//this is a little janky ngl
-			this.itemInHands.body.position = this.body.position.vadd(new phys.Vec3(0, 1, -0.5));
-		}
-
-		if (movement.jump && this.onGround) {
-			const deltaVy = new phys.Vec3(0, JUMP_SPEED, 0).vsub(currentVelocity.vmul(new phys.Vec3(0, 1, 0)));
-			this.body.applyImpulse(deltaVy.scale(this.body.mass));
-		}
-	}
-
-	override onCollide(otherEntity: Entity): void {}
-
-	serialize(): SerializedEntity {
-		return {
-			name: this.name,
-			model: this.model,
-			position: this.body.position.toArray(),
-			quaternion: this.body.quaternion.toArray(),
-			colliders: [
-				{
-					type: "cylinder",
-					radiusTop: this.cylinder.radiusTop,
-					radiusBottom: this.cylinder.radiusBottom,
-					height: this.cylinder.height,
-					numSegments: this.cylinder.numSegments,
-					offset: [0, -cylinderHeight / 2, 0],
-				},
-				{
-					type: "sphere", //capsule Top
-					radius: this.sphereTop.radius,
-				},
-				{
-					type: "sphere", //capsule Bot
-					radius: this.sphereBot.radius,
-					offset: [0, -cylinderHeight, 0],
-				},
-			],
-		};
 	}
 }
