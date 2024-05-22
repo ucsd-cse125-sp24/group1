de3ca2d7c9c6dabb7cac116dac25dbfad67457da
William Wu <dev.wwu@gmail.com>
2024-05-22 16:42:51 -0700

    Remove old toon shaders

--- client/render/engine/GraphicsEngine.ts
+++ client/render/engine/GraphicsEngine.ts
@@ -1,10 +1,7 @@
-import basicVertexSource from "../../shaders/basic.vert";
 import gltfFragmentSource from "../../shaders/gltf.frag";
 import gltfVertexSource from "../../shaders/gltf.vert";
 import wireframeFragmentSource from "../../shaders/wireframe.frag";
 import wireframeVertexSource from "../../shaders/wireframe.vert";
-import toonShaderSouce2 from "../../shaders/toon2.frag";
-import { BoxGeometry } from "../geometries/BoxGeometry";
 import particleFragmentSource from "../../shaders/particle.frag";
 import particleVertexSource from "../../shaders/particle.vert";
 import { getModels } from "../../../assets/models";
@@ -16,14 +13,6 @@ import { WebGlUtils } from "./WebGlUtils";
  * Handles helper functions for interacting with WebGL.
  */
 class GraphicsEngine extends WebGlUtils {
-	tempMaterial = new ShaderProgram(
-		this,
-		this.createProgram(
-			this.createShader("vertex", basicVertexSource, "basic.vert"),
-			this.createShader("fragment", toonShaderSouce2, "toon2.frag"),
-		),
-	);
-	tempGeometry = new BoxGeometry(this.tempMaterial, [1, 1, 1]);
 	wireframeMaterial = new ShaderProgram(
 		this,
 		this.createProgram(
