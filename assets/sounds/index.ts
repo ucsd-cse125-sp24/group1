3ac6ce7b9bb0ffb1eb5c80d2622b71524b1b3d79
Sean <ussa.say@gmail.com>
2024-05-28 02:54:46 -0700

    docs: Document when sounds are played

--- assets/sounds/index.ts
+++ assets/sounds/index.ts
@@ -22,25 +22,54 @@ import trapTriggered from "./trap-triggered.mp3";
 import useFail from "./use-fail.mp3";
 
 export const sounds = {
+	/** Player tries to attack but it's not aimed at anything. */
 	attackFail,
+	/**
+	 * A crafter ejects all items right after absorbing an item because it cannot
+	 * form a valid recipe.
+	 */
 	craftingEjectAll,
+	/** A crafter absorbs an item and is waiting for more items. */
 	craftingPickup,
+	/** A crafter absorbs an item and spits out the result. */
 	craftingSuccess,
+	/**
+	 * Player attacks an interactable object (e.g. item, crafter), which does a
+	 * little knockback.
+	 */
 	defaultHit: [defaultHit1, defaultHit2, defaultHit3],
+	/** Player attacks another player (currently just does knockback). */
 	hit,
+	/** Player jumps. */
 	jump,
+	/** A hero picks up an item. */
 	pickup,
+	/** Attempting to remove an item stored in an empty crafter. */
 	popCraftingFail,
+	/** Removing an item stored in a crafter. */
 	popCrafting,
+	/** Correct tool used on a spawner, spawning an item. */
 	spawnerHarvest,
+	/** Incorrect tool used on a spawner. */
 	spawnerReject,
+	/** A boss spores a hero. */
 	spore,
+	/** Played when a hero throws an item they're holding. */
 	throw: throwSound,
+	/** Someone deletes a trap that isn't trapping a player */
 	trapDisarm,
+	/**
+	 * A hero is freed from the trap. The trap disappears. It's played
+	 * alongside `trapHit` (but we can change this).
+	 */
 	trapEscape,
+	/** Trapped hero hits the trap. It needs multiple hits to free the hero. */
 	trapHit,
+	/** Boss spawns a trap. */
 	trapPlace,
+	/** Hero steps onto a trap. */
 	trapTriggered,
+	/** Player tries to use but it doesn't do anything. */
 	useFail,
 };
 
