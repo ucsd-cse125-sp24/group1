7205f5226bdba51c8362ab1f7f79e971caa836ae
Sean <ussa.say@gmail.com>
2024-06-02 21:47:52 -0700

    feat: Show health as donuts around player

--- server/entities/PlayerEntity.ts
+++ server/entities/PlayerEntity.ts
@@ -275,12 +275,18 @@ export abstract class PlayerEntity extends Entity {
 					rotation: [0, Math.SQRT1_2, 0, Math.SQRT1_2],
 					font: { weight: "bold" },
 				},
-				{
-					text: `${this.health.toFixed(1)} HP`,
-					height: 0.2,
-					offset: [0, 0.5, 0],
-					rotation: [0, Math.SQRT1_2, 0, Math.SQRT1_2],
-				},
+				...Array.from(
+					{ length: this.health },
+					(_, i): EntityModel => ({
+						modelId: "donut",
+						scale: 0.3,
+						offset: [
+							Math.cos((i / this.health + Date.now() / 10000) * 2 * Math.PI) * 0.2 * (this.health - 1),
+							0.5 + Math.cos((i / this.health + Date.now() / 5000) * 2 * Math.PI) * 0.05,
+							Math.sin((i / this.health + Date.now() / 10000) * 2 * Math.PI) * 0.2 * (this.health - 1),
+						],
+					}),
+				),
 			],
 			health: this.health,
 		};
