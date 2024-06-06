056d5befc4e471576bc8176e450531c4bcd63a60
Sean <ussa.say@gmail.com>
2024-06-06 08:17:03 -0700

    refactor: prioritize recycling small capacity particlesystems

--- client/render/model/ParticleManager.ts
+++ client/render/model/ParticleManager.ts
@@ -4,10 +4,8 @@ import GraphicsEngine from "../engine/GraphicsEngine";
 import { ParticleSystem } from "./ParticleSystem";
 
 /**
- * Plays sounds. Permits playing multiple instances of the same sound, and
- * recycles `Audio` objects.
- *
- * Code taken from https://nolanchai.dev/Commit-Challenge-2024/spamsound.html
+ * Recycles `ParticleSystem` objects to play multiple particles at once without
+ * consuming unnecessary memory.
  */
 export class ParticleManager {
 	#engine: GraphicsEngine;
@@ -17,9 +15,20 @@ export class ParticleManager {
 		this.#engine = engine;
 	}
 
+	/**
+	 * Enables a `ParticleSystem` object. It first tries to use a disabled
+	 * `ParticleSystem` object that has sufficient capacity
+	 */
 	create(options: Partial<ParticleOptions>): void {
 		const particleCount = options.spawnCount ?? 1;
-		let particleSystem;
+		// Finds an available ParticleSystem with the smallest capacity
+		let particleSystem = this.#particles.reduce<ParticleSystem | null>(
+			(cum, curr) =>
+				!curr.enabled && curr.maxParticles >= particleCount && (!cum || curr.maxParticles < cum.maxParticles)
+					? curr
+					: cum,
+			null,
+		);
 		for (const particles of this.#particles) {
 			if (particles.enabled || particles.maxParticles < particleCount) {
 				continue;
@@ -35,6 +44,10 @@ export class ParticleManager {
 		particleSystem.enable();
 	}
 
+	/**
+	 * Draws all the enabled particles, as well as disabling (marking as reusable)
+	 * particles that should've expired (per `ttl`).
+	 */
 	paint(view: mat4) {
 		this.#engine.particleShader.use();
 		this.#engine.gl.uniformMatrix4fv(this.#engine.particleShader.uniform("u_view"), false, view);
