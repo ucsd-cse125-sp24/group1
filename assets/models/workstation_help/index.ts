d7c2be22df68411e63fe163458da2e57fe9a66d5
Sean <ussa.say@gmail.com>
2024-06-07 15:22:35 -0700

    feat: add more help

--- assets/models/workstation_help/index.ts
+++ assets/models/workstation_help/index.ts
@@ -2,4 +2,8 @@ import { parseGltf } from "../../../common/gltf/gltf-parser";
 import bin from "./helphelphelp.bin";
 import root from "./helphelphelp.gltf";
 import png from "./workstation_recipes.png";
+import png2 from "./anvil_recipes.png";
+import furnace from "./furnace_recipes.png";
 export const workstationHelp = parseGltf(root, { "helphelphelp.bin": bin, "workstation_recipes.png": png });
+export const anvilHelp = parseGltf(root, { "helphelphelp.bin": bin, "workstation_recipes.png": png2 });
+export const furnaceHelp = parseGltf(root, { "helphelphelp.bin": bin, "workstation_recipes.png": furnace });
